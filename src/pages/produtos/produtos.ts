a66b06a24daaed20a6b0d55d350384e668d7df1f
gabriel <gabrielmqs04@gmail.com>
2018-12-03 22:36:06 -0200

    aula 134 - carregando imagens dos produtos
    atualizando produto.ts para carregar imagens
    adicionando função que busca imagem no s3 em produto service

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
+import { apiConfig } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -23,9 +24,24 @@ export class ProdutosPage {
     let categoriaId = this.navParams.get('categoriaId'); // pega o parametro da página
     this.produtoService
       .findByCategoria(categoriaId)
-      .subscribe(
-        res => this.items = res['content'], 
-        err => {} 
-      );
+      .subscribe(res => {
+        this.items = res['content']
+        this.loadImageUrls();
+      }, 
+      err => {} 
+    );
   };
+
+  loadImageUrls() {
+    for (let i = 0; this.items.length; i++) {
+      let item = this.items[i];
+
+      this.produtoService
+        .getSmallImageFromBucket(item.id)
+        .subscribe(
+          res => item.imageUrl = `${apiConfig.bucketBaseUrl}/prod${item.id}-small.jpg`,
+          err => {}
+        );
+    }
+  }
 }
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Rx';
+
 import { apiConfig } from "../../config/api.config";
 
 @Injectable()
@@ -9,5 +11,9 @@ export class ProdutoService {
 
   findByCategoria(categoriaId: string) {
     return this.http.get(`${apiConfig.baseUrl}/produtos/?categorias=${categoriaId}`);
+  }
+
+  getSmallImageFromBucket(id: string): Observable<any> {
+    return this.http.get(`${apiConfig.bucketBaseUrl}/prod${id}-small.jpg`, { responseType: 'blob' });
   }
 }
